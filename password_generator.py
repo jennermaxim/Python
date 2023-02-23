90551934a6a49a3f15b5c472ecd0ff8ede6376e3
Jenner-Maxim <117904654+Jenner-Maxim@users.noreply.github.com>
2023-02-23 13:38:36 +0300

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+# Python program to generator password
+
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "[]{}()*;/,._-+$!@#%^&"
+
+all = lower + upper + numbers + symbols
+length = 10
+password = "".join(random.sample(all,length))
+
+print(password)
